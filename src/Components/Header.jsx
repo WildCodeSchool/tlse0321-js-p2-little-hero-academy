3cf3513c3f79f6b7d98d06c4152335b99f39495e
ecoffet <simon.ecoffet@gmail.com>
2021-04-14 15:27:12 +0200

    add css nav-bar header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,22 +6,24 @@ import logo from '../img/logo.png';
 const header = () => (
   <div className="navigation">
     <img className="logo" src={logo} alt="logo-little-hero-academy" />
-    <nav>
-      <ul>
-        <li>
-          <a href="#Jeux">Jeux</a>
-        </li>
-        <li>
-          <a href="#Apropos">À propos</a>
-        </li>
-        <li>
-          <a href="#Contact">Contact</a>
-        </li>
-      </ul>
+    <div className="nav-container">
       <button type="button" className="humberger-btn">
         <img className="humberger" src={humberger} alt="humberger-menu-logo" />
       </button>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <a href="#Jeux">Jeux</a>
+          </li>
+          <li>
+            <a href="#Apropos">À propos</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 );
 

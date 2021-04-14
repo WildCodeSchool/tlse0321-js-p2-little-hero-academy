ad060367508c9df6522f7866ddbe26bb18f0f361
Lucciardi <charlotte.lucciardi@gmail.com>
2021-04-14 15:27:10 +0200

    modofication de Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Quiz from './Components/Game';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <p>Hello Team</p>
+      <Quiz />
       <Footer />
     </div>
   );

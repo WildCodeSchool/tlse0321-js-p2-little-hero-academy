2bd4aec2e54b40a42f4e326727fca8e4ae46c9ad
ecoffet <simon.ecoffet@gmail.com>
2021-04-14 16:20:06 +0200

    add toggle for humberger menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Header.css';
 import humberger from '../img/humberger-menu.png';
 import logo from '../img/logo.png';
 
-const header = () => (
-  <div className="navigation">
-    <img className="logo" src={logo} alt="logo-little-hero-academy" />
-    <div className="nav-container">
-      <button type="button" className="humberger-btn">
-        <img className="humberger" src={humberger} alt="humberger-menu-logo" />
-      </button>
-      <nav>
-        <ul>
-          <li>
-            <a href="#Jeux">Jeux</a>
-          </li>
-          <li>
-            <a href="#Apropos">À propos</a>
-          </li>
-          <li>
-            <a href="#Contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+const header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+  return (
+    <div className="navigation">
+      <img className="logo" src={logo} alt="logo-little-hero-academy" />
+      <div className="nav-container">
+        <button type="button" className="humberger-btn" onClick={handleToggle}>
+          <img
+            className="humberger"
+            src={humberger}
+            alt="humberger-menu-logo"
+          />
+        </button>
+        <nav>
+          <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
+            <li>
+              <a
+                href="#Jeux"
+                activeClassName="active-link"
+                onClick={() => closeMenu()}
+                exact
+              >
+                Jeux
+              </a>
+            </li>
+            <li>
+              <a
+                href="#Apropos"
+                activeClassName="active-link"
+                onClick={() => closeMenu()}
+                exact
+              >
+                À propos
+              </a>
+            </li>
+            <li>
+              <a
+                href="#Contact"
+                activeClassName="active-link"
+                onClick={() => closeMenu()}
+                exact
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default header;

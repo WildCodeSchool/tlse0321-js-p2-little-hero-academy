124224036b5fd6973389ad695b9a0865104b8a8f
ecoffet <simon.ecoffet@gmail.com>
2021-04-14 11:25:20 +0200

    add jsx in header and flexbox for good disposition

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 import '../Styles/Header.css';
 
 const header = () => (
-  <div>
-    <p>Header</p>
+  <div className="navigation">
+    <nav>
+      <h1>Little Hero Academy</h1>
+      <ul>
+        <li>
+          <a href="#Jeux">Jeux</a>
+        </li>
+        <li>
+          <a href="#Apropos">À propos</a>
+        </li>
+        <li>
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 );
 

ab4eb79bdd543092d86bee9cf88eff5ddccadfe3
jeromrBor <jerome.borga@gmail.com>
2021-04-14 15:31:23 +0200

    changed a propos with credits, still WIP

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,43 +4,85 @@ import '../App.css';
 
 const Footer = () => (
   <div className="site-footer">
-    <div className="footer-about">
-      <h6>A propos</h6>
-      <p>
-        Notre Little Hero Academy est un site de jeu ludique et intéractif pour
-        enfants dans le monde des super-héros de tous les univers regroupés. Ce
-        site est mise à jour régulierement afin d&apos;apporter du nouveaux
-        contenu.
-      </p>
+    <div className="footer-container">
+      <div className="footer-about">
+        <h6>Crédits</h6>
+        <ul>
+          <li>
+            Vecteurs par{' '}
+            <a
+              href="https://waww.vecteezy.com/free-vector/cartoon"
+              className="href-footer"
+            >
+              Vecteezy
+            </a>{' '}
+            et{' '}
+            <a
+              href="https://www.freepik.com/vectors/background"
+              className="href-footer"
+            >
+              www.freepik.com
+            </a>
+          </li>
+          <li>
+            Typographie{' '}
+            <a href="https://fonts.google.com/" className="href-footer">
+              GoogleFonts
+            </a>{' '}
+            et{' '}
+            <a href="https://www.dafont.com/" className="href-footer">
+              Dafont
+            </a>
+          </li>
+          <li>
+            Avatar cartoon fait sur{' '}
+            <a
+              href="https://superherotar.framiq.com/fr/"
+              className="href-footer"
+            >
+              Superherotar
+            </a>{' '}
+          </li>
+        </ul>
+      </div>
+      <div className="footer-social-link">
+        <h6>Suivez-nous</h6>
+        <ul>
+          <li>
+            <a href="www.facebook.com" className="href-footer">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="wwww.twitter.com" className="href-footer">
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a href="wwww.instragram.com" className="href-footer">
+              Instragral
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-quick-links">
+        <h6>Lien rapide</h6>
+        <ul>
+          <li>Notre histoire</li>
+          <li>Livre d&apos;Or</li>
+          <li>Nous contacter</li>
+          <li>Plan du site</li>
+        </ul>
+      </div>
     </div>
-    <div className="footer-social-link">
-      <h6>Suivez-nous</h6>
-      <ul>
-        <li>
-          <a href="www.facebook.com" className="href-footer">
-            Facebook
-          </a>
-        </li>
-        <li>
-          <a href="wwww.twitter.com" className="href-footer">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a href="wwww.instragram.com" className="href-footer">
-            Instragral
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div className="footer-quick-links">
-      <h6>Lien rapide</h6>
-      <ul>
-        <li>Notre histoire</li>
-        <li>Livre d&apos;Or</li>
-        <li>Nous contacter</li>
-        <li>Plan du site</li>
-      </ul>
+    <div className="separator">{null}</div>
+    <div className="footer-credits">
+      <div className="copyrights">
+        <p>
+          LITTLE HERO ACADEMY. Site crée par les élèves de la Wild Code School
+          de Toulouse session mars 2021. Aucun droits réservés.
+        </p>
+      </div>
     </div>
   </div>
 );

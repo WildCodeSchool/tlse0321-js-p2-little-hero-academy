8e46892c5757f558c72dfa0c1f5655ef069855a1
ecoffet <simon.ecoffet@gmail.com>
2021-04-15 11:11:36 +0200

    add carousel component and style

---
+++ src/Components/Carousel.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Styles/Carousel.css';
+
+const Carousel = () => {
+  const myCarousel = [];
+  console.log(myCarousel);
+
+  return (
+    <div>
+      <h1>Carrousel</h1>
+    </div>
+  );
+};
+
+export default Carousel;

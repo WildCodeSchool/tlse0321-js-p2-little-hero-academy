7d09d8aeb24c248f635142df827e8012889490a8
ecoffet <simon.ecoffet@gmail.com>
2021-04-16 16:21:05 +0200

    corectif orthograph

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -33,7 +33,7 @@ const header = () => {
         <nav>
           <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
             <Link closeMenu={closeMenu} label="jeux" href="#jeux" />
-            <Link closeMenu={closeMenu} label="À props" href="#apropos" />
+            <Link closeMenu={closeMenu} label="À propos" href="#apropos" />
             <Link closeMenu={closeMenu} label="contact" href="#contact" />
           </ul>
         </nav>

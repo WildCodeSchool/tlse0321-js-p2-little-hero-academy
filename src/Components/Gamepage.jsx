fa57c3e29759662dc3b21714fe18f38e2bc8acca
jeromrBor <jerome.borga@gmail.com>
2021-04-16 12:32:41 +0200

    still WIP placed elments

--- src/Components/Gamepage.jsx
+++ src/Components/Gamepage.jsx
@@ -3,23 +3,18 @@ import '../Styles/Gamepage.css';
 
 const Gamepage = () => (
   <div className="gamepage">
-    <h2 className="gamepage-title">Bienvenue sur le jeu du #####, Héro !</h2>
     <div className="gamepage-container">
+      <h2 className="gamepage-title">Bienvenue sur le jeu du ##### !</h2>
       <div className="game-rules">
-        <div className="text-rules">
-          <h4 className="rules-title">Voici les règles du jeu : </h4>
-          <ul>
-            <li>Règle 1</li>
-            <li>Régle 2</li>
-            <li>Règle 3</li>
-          </ul>
-        </div>
         <div className="rules-heropicture-container">
           <img
             src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
             alt="hero"
             className="rules-heropicture"
           />
+        </div>
+        <div className="rules-text">
+          Did someone say <em>chimichangas?</em>
         </div>
       </div>
     </div>

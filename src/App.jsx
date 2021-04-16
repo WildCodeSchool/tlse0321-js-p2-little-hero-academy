ae1aa4e7ed60525b92f61146af0823bd173337cf
ecoffet <simon.ecoffet@gmail.com>
2021-04-16 10:33:09 +0200

    add carousel whis external lib and add ruls eslint for accept external libs

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import './Styles/reset.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Carousel from './Components/Carousel';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Carousel />
       <p>Hello Team</p>
       <Footer />
     </div>
--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../Styles/Carousel.css';
+import AliceCarousel from 'react-alice-carousel';
 
-const Carousel = () => {
-  const myCarousel = [];
-  console.log(myCarousel);
-
-  return (
-    <div>
-      <h1>Carrousel</h1>
-    </div>
-  );
-};
+const Carousel = () => (
+  <div className="carousel">
+    <AliceCarousel autoPlay duration={4000}>
+      <img src="#img" className="yours-custom-class" alt="img" />
+      <img src="#img2" className="yours-custom-class" alt="img" />
+      <img src="/img3" className="yours-custom-class" alt="img" />
+      <img src="/img4" className="yours-custom-class" alt="img" />
+      <img src="/img5" className="yours-custom-class" alt="img" />
+    </AliceCarousel>
+  </div>
+);
 
 export default Carousel;

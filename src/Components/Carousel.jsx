c48529ede696a8c23a546a0a6e63970c5dd5aa1c
ecoffet <simon.ecoffet@gmail.com>
2021-04-16 10:59:53 +0200

    add carousel rules for auto play and remove ruls eslint for accept external libs and move devDependance ruls in dependance in package.json

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -4,7 +4,15 @@ import AliceCarousel from 'react-alice-carousel';
 
 const Carousel = () => (
   <div className="carousel">
-    <AliceCarousel autoPlay duration={4000}>
+    <AliceCarousel
+      duration={400}
+      autoPlay
+      startIndex={1}
+      fadeOutAnimation
+      mouseDragEnabled
+      playButtonEnabled
+      autoPlayInterval={2000}
+    >
       <img src="#img" className="yours-custom-class" alt="img" />
       <img src="#img2" className="yours-custom-class" alt="img" />
       <img src="/img3" className="yours-custom-class" alt="img" />

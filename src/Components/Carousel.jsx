886ea5955cb0e6155f546e9f525516e7a4201346
ecoffet <simon.ecoffet@gmail.com>
2021-04-18 09:45:40 +0200

    modif style for carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -4,17 +4,20 @@ import AliceCarousel from 'react-alice-carousel';
 
 const Carousel = () => (
   <div className="carousel">
-    <h1>Choisis ton jeux</h1>
+    <div className="headerCarousel">
+      <h1>Choisis ton jeux</h1>
+    </div>
     <div className="carouselWindow">
       <AliceCarousel
         className="carouselBox"
-        duration={400}
+        duration={600}
         autoPlay
+        infinite
         startIndex={1}
         fadeOutAnimation
         mouseDragEnabled
         playButtonEnabled
-        autoPlayInterval={2000}
+        autoPlayInterval={5000}
       >
         <img
           src="https://images.unsplash.com/photo-1572541859037-2d4c68076e12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80"
@@ -22,12 +25,12 @@ const Carousel = () => (
           alt="img"
         />
         <img
-          src="https://images.unsplash.com/photo-1572541859037-2d4c68076e12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80"
+          src="https://images.unsplash.com/photo-1600480505021-e9cfb05527f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1346&q=80"
           className="yours-custom-class"
           alt="img"
         />
         <img
-          src="https://images.unsplash.com/photo-1572541859037-2d4c68076e12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80"
+          src="https://images.unsplash.com/photo-1569003339405-ea396a5a8a90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
           className="yours-custom-class"
           alt="img"
         />

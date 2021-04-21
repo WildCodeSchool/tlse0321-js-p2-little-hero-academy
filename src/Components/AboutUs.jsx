579d1a9b158fa4208a322b79fb890d2efe82c249
jeromrBor <jerome.borga@gmail.com>
2021-04-21 14:04:29 +0200

    created file css and jsx for aboutus page

---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Styles/AboutUs.css';
+
+const AboutUs = () => (
+  <div className="about-us">
+    <h1 className="about-us-title">Game title</h1>
+  </div>
+);
+
+export default AboutUs;

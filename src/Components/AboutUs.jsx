d0d0583cddb7901c20ca287a7770c51a1cb6edac
jeromrBor <jerome.borga@gmail.com>
2021-04-22 11:45:49 +0200

    improved cards fopr about us with sample text, still WIP and has to put members text for each members of group

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -14,7 +14,7 @@ const AboutUs = () => (
       <div className="aboutus-card" href="recettespages/recette-page.html">
         <div className="aboutus-img-container">
           <img
-            className="abouts-img"
+            className="aboutus-img"
             src="https://thumbs.dreamstime.com/b/superhero-kid-against-blue-sky-background-girl-power-concept-31740105.jpg"
             alt="recette des cookies chocolat"
           />
@@ -33,28 +33,121 @@ const AboutUs = () => (
         </div>
         <table className="aboutus-stats">
           <tr>
-            <td>
-              <i className="fas fa-stopwatch" />
-              &ensp;37 Force
-            </td>
-            <td className="stats-separator">
-              <i className="fas fa-square" />
-            </td>
-            <td>
-              <i className="fas fa-utensils" />
-              &ensp;67 inteligence
-            </td>
-            <td className="stats-separator">
-              <i className="fas fa-square" />
-            </td>
-            <td>
-              <i className="fas fa-coins" />
-              &ensp;66 rapiditié
-            </td>
-            <td>
-              <i className="fas fa-coins" />
-              &ensp;84 flemme
-            </td>
+            <td>67</td>
+            <td>43</td>
+            <td>72</td>
+            <td>99</td>
+          </tr>
+          <tr>
+            <td>INT</td>
+            <td>STR</td>
+            <td>DEX</td>
+            <td>FLEMME</td>
+          </tr>
+        </table>
+      </div>
+      <div className="aboutus-card" href="recettespages/recette-page.html">
+        <div className="aboutus-img-container">
+          <img
+            className="aboutus-img"
+            src="https://thumbs.dreamstime.com/b/superhero-kid-against-blue-sky-background-girl-power-concept-31740105.jpg"
+            alt="recette des cookies chocolat"
+          />
+        </div>
+        <div className="aboutus-card-title">
+          <h2>
+            Charlotte,
+            <br /> super chat
+          </h2>
+        </div>
+        <div className="aboutus-card-text">
+          Son pouvoir est immense ! Il a la capacité de dormir plus de 10h
+          d&apos;affilées à moins qu&apos;on ne le reveille. Et en plus, il
+          n&apos;est pas du tout du matin ! Il est super, c&apos;est un héro,
+          c&apos;est un super hero !!
+        </div>
+        <table className="aboutus-stats">
+          <tr>
+            <td>67</td>
+            <td>43</td>
+            <td>72</td>
+            <td>99</td>
+          </tr>
+          <tr>
+            <td>INT</td>
+            <td>STR</td>
+            <td>DEX</td>
+            <td>FLEMME</td>
+          </tr>
+        </table>
+      </div>
+      <div className="aboutus-card" href="recettespages/recette-page.html">
+        <div className="aboutus-img-container">
+          <img
+            className="aboutus-img"
+            src="https://thumbs.dreamstime.com/b/superhero-kid-against-blue-sky-background-girl-power-concept-31740105.jpg"
+            alt="recette des cookies chocolat"
+          />
+        </div>
+        <div className="aboutus-card-title">
+          <h2>
+            Simon,
+            <br /> super codeur
+          </h2>
+        </div>
+        <div className="aboutus-card-text">
+          Son pouvoir est immense ! Il a la capacité de dormir plus de 10h
+          d&apos;affilées à moins qu&apos;on ne le reveille. Et en plus, il
+          n&apos;est pas du tout du matin ! Il est super, c&apos;est un héro,
+          c&apos;est un super hero !!
+        </div>
+        <table className="aboutus-stats">
+          <tr>
+            <td>67</td>
+            <td>43</td>
+            <td>72</td>
+            <td>99</td>
+          </tr>
+          <tr>
+            <td>INT</td>
+            <td>STR</td>
+            <td>DEX</td>
+            <td>FLEMME</td>
+          </tr>
+        </table>
+      </div>
+      <div className="aboutus-card" href="recettespages/recette-page.html">
+        <div className="aboutus-img-container">
+          <img
+            className="aboutus-img"
+            src="https://thumbs.dreamstime.com/b/superhero-kid-against-blue-sky-background-girl-power-concept-31740105.jpg"
+            alt="recette des cookies chocolat"
+          />
+        </div>
+        <div className="aboutus-card-title">
+          <h2>
+            Kevin,
+            <br /> super pauseur
+          </h2>
+        </div>
+        <div className="aboutus-card-text">
+          Son pouvoir est immense ! Il a la capacité de dormir plus de 10h
+          d&apos;affilées à moins qu&apos;on ne le reveille. Et en plus, il
+          n&apos;est pas du tout du matin ! Il est super, c&apos;est un héro,
+          c&apos;est un super hero !!
+        </div>
+        <table className="aboutus-stats">
+          <tr>
+            <td>67</td>
+            <td>43</td>
+            <td>72</td>
+            <td>99</td>
+          </tr>
+          <tr>
+            <td>INT</td>
+            <td>STR</td>
+            <td>DEX</td>
+            <td>FLEMME</td>
           </tr>
         </table>
       </div>

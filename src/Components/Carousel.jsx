e24699016a0d6d3407a1078467792da8af50fe76
Lucciardi <charlotte.lucciardi@gmail.com>
2021-04-25 00:03:40 +0200

    routes des jeux vers carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -25,6 +25,7 @@ const Carousel = () => (
             src="https://images.unsplash.com/photo-1572541859037-2d4c68076e12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80"
             className="yours-custom-class"
             alt="img"
+            to
           />
           <div className="centred">
             <h1>Quiz</h1>
@@ -35,6 +36,7 @@ const Carousel = () => (
             src="https://images.unsplash.com/photo-1600480505021-e9cfb05527f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1346&q=80"
             className="yours-custom-class"
             alt="img"
+            to
           />
           <div className="centred">
             <h1>Sauvetage</h1>

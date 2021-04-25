eaaae3459c952c1580c978b1d9b46f2669b11153
jeromrBor <jerome.borga@gmail.com>
2021-04-25 17:08:31 +0200

    improved css of header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -17,29 +17,19 @@ const header = () => {
       setNavbarOpen(false);
     }
   };
+
   window.addEventListener('resize', redimensionnement);
 
   return (
     <div className="header">
-      <nav>
+      <nav className="navbar">
         <img className="logo" src={logo} alt="logo-little-hero-academy" />
         <ul className={`liste-nav ${navbarOpen ? ' showMenu' : ''}`}>
-          <Link closeMenu={closeMenu} to="/jeux" label="Jeux" href="jeux" />
-          <Link closeMenu={closeMenu} to="/quiz" label="Quiz" href="quiz" />
-          <Link
-            closeMenu={closeMenu}
-            to="/aboutus"
-            label="À propos"
-            href="apropos"
-          />
-          <Link closeMenu={closeMenu} to="/pendu" label="Pendu" href="Pendu" />
-          <Link closeMenu={closeMenu} label="À propos" href="apropos" />
-          <Link
-            closeMenu={closeMenu}
-            to="/contactform"
-            label="Contact"
-            href="contact"
-          />
+          <Link to="/jeux" closeMenu={closeMenu} label="Jeux" />{' '}
+          <Link to="/quiz" closeMenu={closeMenu} label="Quiz" />{' '}
+          <Link to="/aboutus" closeMenu={closeMenu} label="À propos" />
+          <Link to="/pendu" closeMenu={closeMenu} label="Pendu" />{' '}
+          <Link to="/contactform" closeMenu={closeMenu} label="Contact" />
         </ul>
         <button type="button" className="hamburger-btn" onClick={handleToggle}>
           <img

89c8f8b89c2e1fa9f9ffa1c9f4df091f535913bb
ecoffet <simon.ecoffet@gmail.com>
2021-04-28 15:00:08 +0200

    logic api

--- src/Components/QuizGame.jsx
+++ src/Components/QuizGame.jsx
@@ -4,6 +4,47 @@ import Gamepage from './Gamepage';
 import Quiz from './quiz-components/Quiz';
 
 const QuizGame = () => {
+  // const [modifyQuestion, setModifyQuestion] = useState([...questions]);
+  // const urlHero = `https://www.superheroapi.com/api.php/10226046072486283/${346}`;
+  // const [imgPersonnage, setImgPersonnage] = useState('');
+  // const [characterName, setCharacterName] = useState('');
+  // const [characterFullName, setCharacterFullName] = useState('');
+  // const [characterGender, setCharacterGender] = useState('');
+  // const [characterAlignment, setCharacterAlignment] = useState('');
+  // const [characterRace, setCharacterRace] = useState('');
+  // const [characterPlaceOfBirth, setCharacterPlaceOfBirth] = useState('');
+  // const [characterPublisher, setCharacterPublisher] = useState('');
+  // const [characterFirstAppearance, setCharacterFirstAppearance] = useState('');
+  // useEffect(() => {
+  //   const newQuestion = [...questions];
+  //   newQuestion[0].answerOptions[2].answerText = characterName;
+  //   newQuestion[1].answerOptions[1].answerText = characterFullName;
+  //   newQuestion[2].answerOptions[1].answerText = characterGender;
+  //   newQuestion[3].answerOptions[0].answerText = characterAlignment;
+  //   newQuestion[4].answerOptions[0].answerText = characterRace;
+  //   newQuestion[5].answerOptions[3].answerText = characterPlaceOfBirth;
+  //   newQuestion[6].answerOptions[2].answerText = characterPublisher;
+  //   newQuestion[7].answerOptions[3].answerText = characterFirstAppearance;
+  //   setModifyQuestion(newQuestion);
+  // }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(urlHero)
+  //     .then((results) => results.data)
+  //     .then((data) => {
+  //       setImgPersonnage(data.image.url);
+  //       setCharacterName(data.name);
+  //       setCharacterFullName(data.biography['full.name']);
+  //       setCharacterGender(data.appearance.gender);
+  //       setCharacterAlignment(data.biography.alignment);
+  //       setCharacterRace(data.biography.race);
+  //       setCharacterPlaceOfBirth(data.biography['place-of-birth']);
+  //       setCharacterPublisher(data.biography.publisher);
+  //       setCharacterFirstAppearance(data.biography['first-appearance']);
+  //     });
+  // }, []);
+
   const gameInformation = {
     name: 'Quiz',
     rules:

b880750b1f0944b601f6d56b47022fd69499b208
jeromrBor <jerome.borga@gmail.com>
2021-04-28 15:03:39 +0200

    API import and random hidden as comment before finishing the quizz random image, and made some adjustement to css and headings

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,43 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../Styles/ContactForm.css';
-import axios from 'axios';
+// import axios from 'axios';
 
-const ContactForm = () => {
-  const [, setImgContact] = useState('');
+const ContactForm = () => (
+  // const [imgPersonnage, setImgPersonnage] = useState('');
+  // const randomImg = Math.floor(Math.random() * 300) + 1;
+  // const urlHero = `https://www.superheroapi.com/api.php/10226046072486283/${randomImg}`;
 
-  const randomImg = Math.floor(Math.random() * 300) + 1;
+  // axios
+  //   .get(urlHero)
+  //   .then((results) => results.data)
+  //   .then((data) => {
+  //     setImgPersonnage(data.image.url);
+  //   });
 
-  const urlHero = `https://www.superheroapi.com/api.php/10226046072486283/${randomImg}/images`;
-
-  axios
-    .get(urlHero)
-    .then((results) => results.data)
-    .then((data) => {
-      setImgContact(data.image.url);
-    });
-
-  return (
-    <article className="contact-form-container">
-      <div className="contact-form-img">
-        <img src={urlHero} alt="blabla" />
-      </div>
-      <form id="contact" method="post">
-        <h1 className="h1">Nous contacter</h1>
-        <h4>Un retour, une suggestion ou un commentaire ?</h4>
-        <input placeholder="Votre nom" type="text" tabIndex="-3" required />
-        <input placeholder="Votre Email" type="email" tabIndex="-2" required />
-        <textarea placeholder="Votre message ici..." tabIndex="-1" required />
-        <button
-          name="submit"
-          type="submit"
-          data-submit="...Sending"
-          className="hvr-sweep-to-bottom"
-          required
-        >
-          Envoyer
-        </button>
-      </form>
-    </article>
-  );
-};
+  <article className="contact-form-container">
+    <div className="contact-form-img">
+      <img src="url" alt="blabla" />
+    </div>
+    <form id="contact" method="post">
+      <h1 className="h1">Nous contacter</h1>
+      <h3>Un retour, une suggestion ou un commentaire ?</h3>
+      <input placeholder="Votre nom" type="text" tabIndex="-3" required />
+      <input placeholder="Votre Email" type="email" tabIndex="-2" required />
+      <textarea placeholder="Votre message ici..." tabIndex="-1" required />
+      <button
+        name="submit"
+        type="submit"
+        data-submit="...Sending"
+        className="hvr-sweep-to-bottom"
+        required
+      >
+        Envoyer
+      </button>
+    </form>
+  </article>
+);
 export default ContactForm;

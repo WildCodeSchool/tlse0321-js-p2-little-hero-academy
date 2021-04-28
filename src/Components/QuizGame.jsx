ab457f787cd6090b31a495b38cc2f73908aab4ba
ecoffet <simon.ecoffet@gmail.com>
2021-04-28 20:44:25 +0200

    remonter de state to parent

--- src/Components/QuizGame.jsx
+++ src/Components/QuizGame.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import '../Styles/Quiz.css';
 import Gamepage from './Gamepage';
 import Quiz from './quiz-components/Quiz';
+import questions from './quiz-components/questions-data';
 
 const QuizGame = () => {
   // const [modifyQuestion, setModifyQuestion] = useState([...questions]);
@@ -20,7 +22,7 @@ const QuizGame = () => {
   // *********=>boucle chaque question is true => changer par state(dones axios) meme [i]
   //   const newQuestion = [...questions];
   //   newQuestion[0].answerOptions[2].answerText = characterName; // exemple(dones.name......)
-  //   newQuestion[1].answerOptions[1].answerText = characterFullName;
+  //   newQuestio[1].answerOptions[1].answerText = characterFullName;
   //   newQuestion[2].answerOptions[1].answerText = characterGender;
   //   newQuestion[3].answerOptions[0].answerText = characterAlignment;
   //   newQuestion[4].answerOptions[0].answerText = characterRace;
@@ -49,15 +51,58 @@ const QuizGame = () => {
 
   // 1 seul requete api pour requperer les donnes
 
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
   const gameInformation = {
     name: 'Quiz',
     rules:
       'Bienvenue dans le Quiz! Dans ce jeu tu dois répondre à une serie de 8 questions. Mieux tu réponds aux questions, plus ta note finale sera élevée. Bonne chance! ',
   };
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    setShowAnswer(true);
+    setTimeout(() => {
+      const nextQuestion = currentQuestion + 1;
+      if (nextQuestion < questions.length) {
+        setCurrentQuestion(currentQuestion + 1);
+        setShowAnswer(false);
+      } else {
+        setShowScore(true);
+      }
+    }, 2000);
+  };
+  const toggleAnswerClass = (isCorrect) => {
+    if (showAnswer) {
+      return isCorrect ? 'correct' : 'incorrect';
+    }
+    return '';
+  };
+  //  ******************************** */
+  const [imgPersonnage, setImgPersonnage] = useState('');
+  const urlHero = `https://www.superheroapi.com/api.php/10226046072486283/${346}`;
+
+  axios
+    .get(urlHero)
+    .then((results) => results.data)
+    .then((data) => {
+      setImgPersonnage(data.image.url);
+    });
   return (
     <div className="game">
       <Gamepage gameInformation={gameInformation} />
-      <Quiz />
+      <Quiz
+        currentQuestion={currentQuestion}
+        showScore={showScore}
+        score={score}
+        setScore={setScore}
+        handleAnswerOptionClick={handleAnswerOptionClick}
+        toggleAnswerClass={toggleAnswerClass}
+        imgPersonnage={imgPersonnage}
+      />
     </div>
   );
 };
--- src/Components/quiz-components/Quiz.jsx
+++ src/Components/quiz-components/Quiz.jsx
@@ -1,88 +1,51 @@
-import React, { useState } from 'react';
-import axios from 'axios';
 import '../../Styles/Quiz.css';
 import GameResult from '../GameResult';
 import questions from './questions-data';
 
-const Question = () => {
-  const [showAnswer, setShowAnswer] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
-
-  const handleAnswerOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
-    }
-
-    setShowAnswer(true);
-    setTimeout(() => {
-      const nextQuestion = currentQuestion + 1;
-      if (nextQuestion < questions.length) {
-        setCurrentQuestion(currentQuestion + 1);
-        setShowAnswer(false);
-      } else {
-        setShowScore(true);
-      }
-    }, 2000);
-  };
-
-  const toggleAnswerClass = (isCorrect) => {
-    if (showAnswer) {
-      return isCorrect ? 'correct' : 'incorrect';
-    }
-    return '';
-  };
-
-  //  ******************************** */
-  const [imgPersonnage, setImgPersonnage] = useState('');
-  const urlHero = `https://www.superheroapi.com/api.php/10226046072486283/${346}`;
-
-  axios
-    .get(urlHero)
-    .then((results) => results.data)
-    .then((data) => {
-      setImgPersonnage(data.image.url);
-    });
-
-  return (
-    <div className="question">
-      {showScore ? (
-        <div className="score-section">
-          <div className="score">
-            Ton score est {score} sur {questions.length}
-          </div>
-          <GameResult victory={score > 5} />
+const Question = ({
+  currentQuestion,
+  showScore,
+  score,
+  handleAnswerOptionClick,
+  toggleAnswerClass,
+  imgPersonnage,
+}) => (
+  <div className="question">
+    {showScore ? (
+      <div className="score-section">
+        <div className="score">
+          Ton score est {score} sur {questions.length}
         </div>
-      ) : (
-        <div className="container">
-          <div className="question-section">
-            <div className="question-count">
-              <span>Question {currentQuestion + 1}</span>/{questions.length}
-            </div>
-            <div className="question-text">
-              {questions[currentQuestion].questionText}
-            </div>
+        <GameResult victory={score > 5} />
+      </div>
+    ) : (
+      <div className="container">
+        <div className="question-section">
+          <div className="question-count">
+            <span>Question {currentQuestion + 1}</span>/{questions.length}
           </div>
-          <div className="answer-section">
-            <div className="image-personnage">
-              <img src={imgPersonnage} alt="personnage" />
-            </div>
-            <div className="answer-area">
-              {questions[currentQuestion].answerOptions.map((answerOption) => (
-                <button
-                  onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
-                  className={toggleAnswerClass(answerOption.isCorrect)}
-                  type="button"
-                >
-                  {answerOption.answerText}
-                </button>
-              ))}
-            </div>
+          <div className="question-text">
+            {questions[currentQuestion].questionText}
           </div>
         </div>
-      )}
-    </div>
-  );
-};
+        <div className="answer-section">
+          <div className="image-personnage">
+            <img src={imgPersonnage} alt="personnage" />
+          </div>
+          <div className="answer-area">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button
+                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+                className={toggleAnswerClass(answerOption.isCorrect)}
+                type="button"
+              >
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    )}
+  </div>
+);
 export default Question;

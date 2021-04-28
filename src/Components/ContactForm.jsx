eb35a1dc2ba55d99d51077ac4a3be41c0b7a15a4
jeromrBor <jerome.borga@gmail.com>
2021-04-28 15:11:36 +0200

    changed some fake text

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -16,14 +16,14 @@ const ContactForm = () => (
 
   <article className="contact-form-container">
     <div className="contact-form-img">
-      <img src="url" alt="blabla" />
+      <img src="url" alt="images" />
     </div>
     <form id="contact" method="post">
       <h1 className="h1">Nous contacter</h1>
       <h3>Un retour, une suggestion ou un commentaire ?</h3>
-      <input placeholder="Votre nom" type="text" tabIndex="-3" required />
-      <input placeholder="Votre Email" type="email" tabIndex="-2" required />
-      <textarea placeholder="Votre message ici..." tabIndex="-1" required />
+      <input placeholder="Votre nom*" type="text" tabIndex="-3" required />
+      <input placeholder="Votre Email*" type="email" tabIndex="-2" required />
+      <textarea placeholder="Votre message ici...*" tabIndex="-1" required />
       <button
         name="submit"
         type="submit"

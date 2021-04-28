e9447503b6e908d0fd2be11419f3837ceeda3b0e
jeromrBor <jerome.borga@gmail.com>
2021-04-28 14:48:31 +0200

    imported api and started to make random math for random image in div

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/ContactForm.css';
+import axios from 'axios';
 
-const ContactForm = () => (
-  <article className="contact-form-container">
-    <div className="contact-form-img">image ici </div>
-    <form id="contact" method="post">
-      <h1 className="h1">Nous contacter</h1>
-      <h4>Un retour, une suggestion ou un commentaire ?</h4>
-      <input placeholder="Votre nom" type="text" tabIndex="-3" required />
-      <input placeholder="Votre Email" type="email" tabIndex="-2" required />
-      <textarea placeholder="Votre message ici..." tabIndex="-1" required />
-      <button
-        name="submit"
-        type="submit"
-        data-submit="...Sending"
-        className="hvr-sweep-to-bottom"
-      >
-        Envoyer
-      </button>
-    </form>
-  </article>
-);
+const ContactForm = () => {
+  const [, setImgContact] = useState('');
+
+  const randomImg = Math.floor(Math.random() * 300) + 1;
+
+  const urlHero = `https://www.superheroapi.com/api.php/10226046072486283/${randomImg}/images`;
+
+  axios
+    .get(urlHero)
+    .then((results) => results.data)
+    .then((data) => {
+      setImgContact(data.image.url);
+    });
+
+  return (
+    <article className="contact-form-container">
+      <div className="contact-form-img">
+        <img src={urlHero} alt="blabla" />
+      </div>
+      <form id="contact" method="post">
+        <h1 className="h1">Nous contacter</h1>
+        <h4>Un retour, une suggestion ou un commentaire ?</h4>
+        <input placeholder="Votre nom" type="text" tabIndex="-3" required />
+        <input placeholder="Votre Email" type="email" tabIndex="-2" required />
+        <textarea placeholder="Votre message ici..." tabIndex="-1" required />
+        <button
+          name="submit"
+          type="submit"
+          data-submit="...Sending"
+          className="hvr-sweep-to-bottom"
+          required
+        >
+          Envoyer
+        </button>
+      </form>
+    </article>
+  );
+};
 export default ContactForm;

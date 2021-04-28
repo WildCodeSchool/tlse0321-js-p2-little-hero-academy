bc1bcb11b8c8605cb5d2e649d72d694a3d8ef4fb
ecoffet <simon.ecoffet@gmail.com>
2021-04-28 21:26:27 +0200

    recuperation via axios de toutes les données du quiz dans un objet

--- src/Components/quiz-components/Quiz.jsx
+++ src/Components/quiz-components/Quiz.jsx
@@ -1,6 +1,5 @@
 import '../../Styles/Quiz.css';
 import GameResult from '../GameResult';
-import questions from './questions-data';
 
 const Question = ({
   currentQuestion,
@@ -8,13 +7,14 @@ const Question = ({
   score,
   handleAnswerOptionClick,
   toggleAnswerClass,
-  imgPersonnage,
+  heroData,
+  modifyQuestion,
 }) => (
   <div className="question">
     {showScore ? (
       <div className="score-section">
         <div className="score">
-          Ton score est {score} sur {questions.length}
+          Ton score est {score} sur {modifyQuestion.length}
         </div>
         <GameResult victory={score > 5} />
       </div>
@@ -22,26 +22,28 @@ const Question = ({
       <div className="container">
         <div className="question-section">
           <div className="question-count">
-            <span>Question {currentQuestion + 1}</span>/{questions.length}
+            <span>Question {currentQuestion + 1}</span>/{modifyQuestion.length}
           </div>
           <div className="question-text">
-            {questions[currentQuestion].questionText}
+            {modifyQuestion[currentQuestion].questionText}
           </div>
         </div>
         <div className="answer-section">
           <div className="image-personnage">
-            <img src={imgPersonnage} alt="personnage" />
+            <img src={[heroData.img]} alt="personnage" />
           </div>
           <div className="answer-area">
-            {questions[currentQuestion].answerOptions.map((answerOption) => (
-              <button
-                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
-                className={toggleAnswerClass(answerOption.isCorrect)}
-                type="button"
-              >
-                {answerOption.answerText}
-              </button>
-            ))}
+            {modifyQuestion[currentQuestion].answerOptions.map(
+              (answerOption) => (
+                <button
+                  onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+                  className={toggleAnswerClass(answerOption.isCorrect)}
+                  type="button"
+                >
+                  {answerOption.answerText}
+                </button>
+              ),
+            )}
           </div>
         </div>
       </div>

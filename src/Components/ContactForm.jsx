ae6ab7e4f0c81a901aeae91ed71b10620756ed93
jeromrBor <jerome.borga@gmail.com>
2021-04-29 11:57:32 +0200

    finished contact form with improved css and responsive !

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -2,30 +2,34 @@ import React from 'react';
 import '../Styles/ContactForm.css';
 
 const ContactForm = () => (
-  <article className="contact-form-container">
-    <div className="contact-form-img">
-      <img
-        id="imgcontact"
-        src="https://www.superherodb.com/pictures2/portraits/10/100/1112.jpg"
-        alt="images"
-      />
-    </div>
-    <form id="contact" method="post">
-      <h1 className="h1">Nous contacter</h1>
-      <h3>Un retour, une suggestion ou un commentaire ?</h3>
-      <input placeholder="Votre nom*" type="text" tabIndex="-3" required />
-      <input placeholder="Votre Email*" type="email" tabIndex="-2" required />
-      <textarea placeholder="Votre message ici...*" tabIndex="-1" required />
-      <button
-        name="submit"
-        type="submit"
-        data-submit="...Sending"
-        className="hvr-sweep-to-bottom"
-        required
-      >
-        Envoyer
-      </button>
-    </form>
+  <article className="contact-form-page">
+    <div className="contact-form-bg" />
+
+    <article className="contact-form-container">
+      <div className="contact-form-img">
+        <img
+          id="imgcontact"
+          src="https://www.superherodb.com/pictures2/portraits/10/100/1112.jpg"
+          alt="images"
+        />
+      </div>
+      <form id="contact" method="post">
+        <h1 className="h1">Nous contacter</h1>
+        <h3>Un retour, une suggestion ou un commentaire ?</h3>
+        <input placeholder="Votre nom*" type="text" tabIndex="-3" required />
+        <input placeholder="Votre Email*" type="email" tabIndex="-2" required />
+        <textarea placeholder="Votre message ici...*" tabIndex="-1" required />
+        <button
+          name="submit"
+          type="submit"
+          data-submit="...Sending"
+          className="hvr-sweep-to-bottom"
+          required
+        >
+          Envoyer
+        </button>
+      </form>
+    </article>
   </article>
 );
 export default ContactForm;

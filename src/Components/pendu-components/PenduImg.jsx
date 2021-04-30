6169b62cbf5ff516b2f385abf50de3b5f7be5dd9
esiomlink <esiomlink@sim.home>
2021-04-30 09:52:53 +0200

    add final img

--- src/Components/pendu-components/PenduImg.jsx
+++ src/Components/pendu-components/PenduImg.jsx
@@ -1,14 +1,12 @@
 import './style/PenduImg.css';
-import img7 from '../../img/7.png';
-import img6 from '../../img/6.png';
-import img5 from '../../img/5.png';
-import img4 from '../../img/4.png';
-import img3 from '../../img/3.png';
-import img2 from '../../img/2.png';
-import img1 from '../../img/1.png';
+import img5 from '../../img/Pendu5.png';
+import img4 from '../../img/Pendu4.png';
+import img3 from '../../img/Pendu3.png';
+import img2 from '../../img/Pendu2.png';
+import img1 from '../../img/Pendu1.png';
 
 const PenduImg = ({ count }) => {
-  const imgArray = [img7, img6, img5, img4, img3, img2, img1];
+  const imgArray = [img5, img4, img3, img3, img2, img1, img1];
   const curentImg = imgArray[count - 1];
   return (
     <div className="sauvetage-img-container">

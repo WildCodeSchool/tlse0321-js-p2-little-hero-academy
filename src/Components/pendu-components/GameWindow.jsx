7cd04b7d59bc2d8d5266a731b2daad564d7a0081
jeromrBor <jerome.borga@gmail.com>
2021-05-04 13:36:16 +0200

    changed html structure and started to change css

--- src/Components/pendu-components/GameWindow.jsx
+++ src/Components/pendu-components/GameWindow.jsx
@@ -28,28 +28,34 @@ const GameWindow = (props) => {
       'Bienvenue dans le jeu du Sauvetage! Ta mission consiste à trouver le nom de notre héros. Si tu y arrives avant que les diplômes aient disparu, tu auras gagné. Bonne chance !',
   };
   return (
-    <section className="window-container">
+    <article className="window-container">
       <Gamepage gameInformation={gameInformation} />
-      <PenduImg count={count} />
-      <Letter showLetter={showLetter} />
-      <HartLife hartFinder={hartFinder} />
-      <Keyboard
-        handleClick={handleClick}
-        alphabet={alphabet}
-        setAlphabet={setAlphabet}
-        count={count}
-        setCount={setCount}
-        word={word}
-      />
-      <NewGame
-        findLetterInWord={findLetterInWord}
-        gameNotYetStarted={gameNotYetStarted}
-      />
-      {gameOver && <Redirect to="/gameoverwindow" label="GameOverWindow" />}
-      {gameWin && (
-        <Redirect to="/gamewinwindow" word={word} label="GameWinWindow" />
-      )}
-    </section>
+      <section className="pendugame-container">
+        <section className="pendugame-life">
+          <PenduImg count={count} />
+          <HartLife hartFinder={hartFinder} />
+        </section>
+        <section className="gamependu-keyboard">
+          <Letter showLetter={showLetter} />
+          <Keyboard
+            handleClick={handleClick}
+            alphabet={alphabet}
+            setAlphabet={setAlphabet}
+            count={count}
+            setCount={setCount}
+            word={word}
+          />
+        </section>
+        <NewGame
+          findLetterInWord={findLetterInWord}
+          gameNotYetStarted={gameNotYetStarted}
+        />
+        {gameOver && <Redirect to="/gameoverwindow" label="GameOverWindow" />}
+        {gameWin && (
+          <Redirect to="/gamewinwindow" word={word} label="GameWinWindow" />
+        )}
+      </section>
+    </article>
   );
 };
 

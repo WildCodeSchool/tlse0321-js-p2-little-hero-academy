5e20ca51d732d8367069afbe4732de0a3734a458
Kevin <kevin.bouaziz1@gmail.com>
2021-05-06 16:02:12 +0200

    modif voix

--- src/Components/VocalRules.jsx
+++ src/Components/VocalRules.jsx
@@ -1,8 +1,8 @@
 function VocalRules({ gameInformation }) {
   const utterance = new SpeechSynthesisUtterance(gameInformation.rules);
   utterance.lang = 'fr-FR';
-  utterance.rate = 1.2;
-  utterance.pitch = 2;
+  utterance.rate = 1.3;
+  utterance.pitch = 0.1;
 
   return (
     <div className="vocal-rules">
